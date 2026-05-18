5783942b38bd7a61a55a42472c1e801db80278be
le10x <leanderdrb@gmail.com>
2026-05-17 21:09:08 -0400

    Modify like behavior in InfiniteLikes.cpp
    Intercept the like trigger to manipulate voting behavior.

--- src/InfiniteLikes.cpp
+++ src/InfiniteLikes.cpp
@@ -4,19 +4,27 @@
 using namespace geode::prelude;
 
 class $modify(MyLikeItemLayer, LikeItemLayer) {
-    // Interceptamos la inicialización del recuadro de Likes
-    bool init(LikeItemType type, int id, bool extra) {
+    // Interceptamos el momento exacto en que se va a enviar el voto al servidor
+    void triggerLike(bool isLike) {
         
-        // Verificamos si el interruptor 'enable-likes' está encendido en mod.json
         bool isEnabled = Mod::get()->getSettingValue<bool>("enable-likes");
 
         if (isEnabled) {
-            // Engañamos al juego forzando que 'extra' (que controla si ya diste voto) sea siempre false.
-            // Esto desbloquea el botón e impide que el juego sepa que ya habías votado.
-            extra = false;
+            auto gsm = GameStatsManager::sharedState();
+            // Verificamos que tengamos acceso a los datos y que la ventana tenga un ID de comentario válido
+            if (gsm && m_itemID != 0) {
+                // Buscamos el diccionario nativo de Cocos2d donde se guardan los elementos votados
+                auto likedDict = gsm->m_likedItems;
+                if (likedDict) {
+                    // Convertimos el ID del comentario a texto
+                    std::string itemKey = std::to_string(m_itemID);
+                    // Borramos el registro para que el juego olvide que ya votaste por él
+                    likedDict->removeObjectForKey(itemKey.c_str());
+                }
+            }
         }
 
-        // Llamamos al inicializador original usando la sintaxis clásica de Geode
-        return LikeItemLayer::init(type, id, extra);
+        // Llamamos a la función original para que envíe el paquete HTTP al servidor
+        LikeItemLayer::triggerLike(isLike);
     }
 };
