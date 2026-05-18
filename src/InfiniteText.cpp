4f6233fc2dfbc78a9736c2b86c9f7de286790f10
le10x <leanderdrb@gmail.com>
2026-05-18 13:16:25 -0400

    Refactor text input handling in InfiniteText
    Removed conditional check for text input enabling.

--- src/InfiniteText.cpp
+++ src/InfiniteText.cpp
@@ -5,14 +5,6 @@ using namespace geode::prelude;
 
 class $modify(MyTextInputNode, CCTextInputNode) {
     bool onTextFieldInsertText(cocos2d::CCTextFieldTTF* sender, char const* text, int length, cocos2d::enumKeyCodes keyCodes) {
-        
-        bool isEnabled = Mod::get()->getSettingValue<bool>("enable-text");
-
-        if (isEnabled) {
-            CCTextInputNode::onTextFieldInsertText(sender, text, length, keyCodes);
-            return false;
-        }
-
         return CCTextInputNode::onTextFieldInsertText(sender, text, length, keyCodes);
     }
 };
