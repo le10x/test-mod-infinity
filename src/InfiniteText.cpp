02010ca0ed7d40ff086a3c7967d8c794eb1371e8
le10x <leanderdrb@gmail.com>
2026-05-18 11:01:37 -0400

    Clean up comments in InfiniteText.cpp
    Removed comments in Spanish for clarity.

--- src/InfiniteText.cpp
+++ src/InfiniteText.cpp
@@ -6,17 +6,13 @@ using namespace geode::prelude;
 class $modify(MyTextInputNode, CCTextInputNode) {
     bool onTextFieldInsertText(cocos2d::CCTextFieldTTF* sender, char const* text, int length, cocos2d::enumKeyCodes keyCodes) {
         
-        // Verificamos si el interruptor 'enable-text' está encendido en los ajustes
         bool isEnabled = Mod::get()->getSettingValue<bool>("enable-text");
 
         if (isEnabled) {
-            // Pasamos los parámetros al juego original
             CCTextInputNode::onTextFieldInsertText(sender, text, length, keyCodes);
-            // Retornamos false para romper el límite nativo
             return false;
         }
 
-        // Si el mod está apagado, se comporta de manera normal de fábrica
         return CCTextInputNode::onTextFieldInsertText(sender, text, length, keyCodes);
     }
 };
