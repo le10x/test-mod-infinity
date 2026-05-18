61a65e6f85a30c587f43b639c8bdc8423e83b7ec
le10x <leanderdrb@gmail.com>
2026-05-18 10:23:26 -0400

    Refactor triggerLike to init for LikeItemLayer

--- src/InfiniteLikes.cpp
+++ src/InfiniteLikes.cpp
@@ -4,27 +4,17 @@
 using namespace geode::prelude;
 
 class $modify(MyLikeItemLayer, LikeItemLayer) {
-    // Interceptamos el momento exacto en que se va a enviar el voto al servidor
-    void triggerLike(bool isLike) {
+    // Interceptamos cuando la ventana de votación se dibuja en pantalla
+    bool init(LikeItemType type, int id, bool extra) {
         
         bool isEnabled = Mod::get()->getSettingValue<bool>("enable-likes");
 
+        // Al forzar que 'extra' sea false, el juego asume que NO has votado.
+        // Al forzar que 'type' sea un tipo genérico externo, se eliminan bloqueos de autoría.
         if (isEnabled) {
-            auto gsm = GameStatsManager::sharedState();
-            // Verificamos que tengamos acceso a los datos y que la ventana tenga un ID de comentario válido
-            if (gsm && m_itemID != 0) {
-                // Buscamos el diccionario nativo de Cocos2d donde se guardan los elementos votados
-                auto likedDict = gsm->m_likedItems;
-                if (likedDict) {
-                    // Convertimos el ID del comentario a texto
-                    std::string itemKey = std::to_string(m_itemID);
-                    // Borramos el registro para que el juego olvide que ya votaste por él
-                    likedDict->removeObjectForKey(itemKey.c_str());
-                }
-            }
+            extra = false; 
         }
 
-        // Llamamos a la función original para que envíe el paquete HTTP al servidor
-        LikeItemLayer::triggerLike(isLike);
+        return LikeItemLayer::init(type, id, extra);
     }
 };
