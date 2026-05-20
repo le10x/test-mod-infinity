40cdf6393f27bcaf4bc777b6f2443f71ec838cc2
le10x <leanderdrb@gmail.com>
2026-05-20 01:10:26 -0400

    Override onTextFieldInsertText to customize input
    Modify text input handling to prevent default behavior.

--- src/InfiniteText.cpp
+++ src/InfiniteText.cpp
@@ -5,6 +5,9 @@ using namespace geode::prelude;
 
 class $modify(MyTextInputNode, CCTextInputNode) {
     bool onTextFieldInsertText(cocos2d::CCTextFieldTTF* sender, char const* text, int length, cocos2d::enumKeyCodes keyCodes) {
-        return CCTextInputNode::onTextFieldInsertText(sender, text, length, keyCodes);
+        
+        CCTextInputNode::onTextFieldInsertText(sender, text, length, keyCodes);
+ 
+        return false;
     }
 };
